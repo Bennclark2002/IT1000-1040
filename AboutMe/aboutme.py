16edcc4add96eb8cbcc21915456ea841a4992e89
Bennclark2002 <92816199+Bennclark2002@users.noreply.github.com>
2021-10-19 13:06:17 -0500

    Add files via upload

---
+++ AboutMe/aboutme.py
@@ -0,0 +1,11 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello, my name is Benjamin Clark")
+Hello, my name is Benjamin Clark
+>>> print("This semester will be my first semester of doing IT!")
+This semester will be my first semester of doing IT!
+>>> print("So far learing pyton has been a blast!")
+So far learing pyton has been a blast!
+>>> print("It has been confusing at times but thats the fun in it!")
+It has been confusing at times but thats the fun in it!
+>>> 
