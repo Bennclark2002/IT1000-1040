563019ab9c05240f95cfba6b354e036ed19083be
Bennclark2002 <92816199+Bennclark2002@users.noreply.github.com>
2021-10-19 13:05:41 -0500

    Add files via upload

---
+++ ObjectPosnCalc/objectposncalc.py
@@ -0,0 +1,6 @@
+x0 = float(input("initial position(x0): "))
+v0 = float(input("initial velocity(v0): "))
+a = float(input("Acceleration(a): "))
+t = float(input("Time(t): "))
+xf = x0 +v0*t + 0.5*a*t*t
+print("\nFinal position(xf): ",xf)
